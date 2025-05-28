b7ac2518e208d8da4ec75ce4e26099177d059124
Robert Nguyen (Bach) <109204757+phpwsoftpro@users.noreply.github.com>
2025-05-28 10:47:19 +0700

    Add Quote CRUD

--- graphql-typegraphql-crud-final/src/index.ts
+++ graphql-typegraphql-crud-final/src/index.ts
@@ -6,12 +6,19 @@ import { TodoResolver } from "./resolvers/TodoResolver";
 import { UserResolver } from "./resolvers/UserResolver";
 import { CompanyResolver } from "./resolvers/CompanyResolver";
 import { ContactResolver } from "./resolvers/ContactResolver";
+import { QuoteResolver } from "./resolvers/QuoteResolver";
 
 const prisma = new PrismaClient();
 
 async function bootstrap() {
   const schema = await buildSchema({
-    resolvers: [TodoResolver, UserResolver, CompanyResolver, ContactResolver],
+    resolvers: [
+      TodoResolver,
+      UserResolver,
+      CompanyResolver,
+      ContactResolver,
+      QuoteResolver,
+    ],
     validate: false,
   });
 
