595da67532eb843aef654d082783e256c9c74fc3
Lê Bá Dũng <lebadung561@gmail.com>
2025-06-28 15:18:13 +0700

    fix build: add products form modal export

---
+++ frontend-graphql/app-crm/src/routes/sales/products/components/products-form-modal.tsx
@@ -0,0 +1,54 @@
+import type { FC } from "react";
+import { Modal, Form, Input } from "antd";
+import { useModalForm } from "@refinedev/antd";
+import { type HttpError, type RedirectAction } from "@refinedev/core";
+import { PRODUCT_CREATE_MUTATION } from "../queries";
+
+type Props = {
+  action: "create" | "edit";
+  redirect?: RedirectAction;
+  onCancel?: () => void;
+  onMutationSuccess?: () => void;
+};
+
+export const ProductsFormModal: FC<Props> = ({
+  action,
+  redirect,
+  onCancel,
+  onMutationSuccess,
+}) => {
+  const { modalProps, formProps, close } = useModalForm<any, HttpError, any>({
+    resource: "products",
+    action,
+    defaultVisible: true,
+    redirect,
+    meta: {
+      gqlMutation: PRODUCT_CREATE_MUTATION,
+    },
+    onMutationSuccess: () => {
+      onMutationSuccess?.();
+    },
+  });
+
+  return (
+    <Modal
+      {...modalProps}
+      onCancel={() => {
+        if (onCancel) {
+          onCancel();
+          return;
+        }
+        close();
+      }}
+    >
+      <Form {...formProps} layout="vertical">
+        <Form.Item name="name" label="Product name" rules={[{ required: true }]}> 
+          <Input />
+        </Form.Item>
+        <Form.Item name="salesPrice" label="Sales Price">
+          <Input type="number" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
